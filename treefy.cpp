88268adf5664f5c56ee0c9d3679263d13af03595
sevity <sevity@ymail.com>
2020-04-15 16:36:49 +0900

    ss

--- treefy.cpp
+++ treefy.cpp
@@ -1,3 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define REP(i,n) for(int i=0;i<(int)(n);i++)
+using vi = vector<int>;
+const int INF = (int)1e9, MAXN = 100001;
+
 //무방향그래프를 인풋으로 받아, 특정 정점을 루트를 가지는 방향그래프(트리)로 만들어준다.
 vector<vi> treefy(vector<vi>& adj_list, int root) {
     vector<vi> tree(adj_list.size());
@@ -11,3 +17,30 @@ vector<vi> treefy(vector<vi>& adj_list, int root) {
     dfs(root, -1);
     return tree;
 }
+
+int N, R, Q, U, V;
+int main()
+{
+    ios::sync_with_stdio(0); cin.tie(0);
+    cin >> N >> R >> Q;
+    vector<vi> adj_list(N + 1);
+    REP(i, N - 1) {
+        cin >> U >> V;
+        adj_list[U].push_back(V);
+        adj_list[V].push_back(U);
+    }
+    vector<vi> tree = treefy(adj_list, R);
+
+    vi dp(MAXN, INF);
+    function<int(int)> cnt_sub = [&](int root) {
+        int& res = dp[root];
+        if (res < INF) return res;
+        int sum = 1; for (auto adj : tree[root]) sum += cnt_sub(adj);
+        return res = sum;
+    };
+    cnt_sub(R);
+
+    while (Q--) cin >> U, cout << dp[U] << '\n';
+    return 0;
+}
+
